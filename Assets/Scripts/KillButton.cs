ad0eabf20d07efc18dd75d5c689189876ff72902
SilentVortex <149170463+SilentVortex@users.noreply.github.com>
2025-01-11 19:49:46 +0000

    final

--- Assets/Scripts/KillButton.cs
+++ Assets/Scripts/KillButton.cs
@@ -2,22 +2,10 @@ using UnityEngine;
 
 public class KillButton : MonoBehaviour
 {
-    // Sets what I want to be able to be changed //
     public Transform player;
     public float attackDamage = 10.0f;
-    void Start()
+    public void Damage()
     {
-        
-    }
-
-    void Update()
-    {
-
-    }
-
-    void Damage()
-    {
-        // Code to handle the attack, such as damaging the player
         player.GetComponent<PlayerHealth>().TakeDamage(attackDamage);
     }
 }
